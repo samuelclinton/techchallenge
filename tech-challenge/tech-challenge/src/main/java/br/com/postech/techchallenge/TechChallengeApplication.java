55641aee12c368a211a269bbb7d872ec1abaf16e
Junior Martins <juniorsoaresmartins@gmail.com>
2023-05-03 05:33:41 -0400

    FEAT - configuração do projeto.

---
+++ tech-challenge/tech-challenge/src/main/java/br/com/postech/techchallenge/TechChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeApplication.class, args);
+	}
+
+}
---
+++ tech-challenge/tech-challenge/src/test/java/br/com/postech/techchallenge/TechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
